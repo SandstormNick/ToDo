62701a3fc303c0f1c8c9dfe9b3dd71cb1a8f69c0
SandstormNick <gibbensnick@gmail.com>
2022-01-15 16:53:29 +0200

    Added Check box for items and changed ItemNameCard to Stateful widget

--- lib/widgets/item_name_card.dart
+++ lib/widgets/item_name_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/item.dart';
 
-class ItemNameCard extends StatelessWidget {
+class ItemNameCard extends StatefulWidget {
   final Item item;
 
   const ItemNameCard({
@@ -11,14 +11,36 @@ class ItemNameCard extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ItemNameCard> createState() => _ItemNameCardState();
+}
+
+class _ItemNameCardState extends State<ItemNameCard> {
+  void _onIsCompletedChanged(bool? newValue) => setState(() {
+        if (newValue == true) {
+          widget.item.isCompleted = true;
+        } else {
+          widget.item.isCompleted = false;
+        }
+      });
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 100,
       child: Card(
         child: Row(
           children: <Widget>[
+            Transform.scale(
+              //scale: 1.5,
+              scale: 1,
+              child: Checkbox(
+                value: widget.item.isCompleted,
+                activeColor: Colors.green,
+                onChanged: _onIsCompletedChanged,
+              ),
+            ),
             Expanded(
-              child: Text(item.itemName),
+              child: Text(widget.item.itemName),
             )
           ],
         ),
